be2b4ab7615188d556b4c34f335873f99d89d0f8
Leonardo Rota-Rossi <leorossi@gmail.com>
2021-09-07 18:27:04 -0400

    feat(initial): Adds support for react on typescript

---
+++ src/app/index.tsx
@@ -0,0 +1,14 @@
+import { FC } from 'react';
+import ReactDOM from 'react-dom';
+
+const App:FC = () => {
+  return (
+    <div>
+      <h2>It works</h2>
+    </div>
+  )
+}
+
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
